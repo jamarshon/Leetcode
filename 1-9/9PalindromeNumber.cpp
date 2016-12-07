d8bfcc4a65c41d1a4fd155163d52d40781fa7415
jamarshon <jasonl96@gmail.com>
2016-12-06 21:30:33 -0500

    Finish 9 Palindrome Number

---
+++ 1-9/9PalindromeNumber.cpp
@@ -0,0 +1,45 @@
+/*
+9. Palindrome Number
+Determine whether an integer is a palindrome. Do this without extra space.
+
+Some hints:
+Could negative integers be palindromes? (ie, -1)
+
+If you are thinking of converting the integer to string, note the restriction of using extra space.
+
+You could also try reversing an integer. However, if you have solved the problem "Reverse Integer", you know that the reversed integer might overflow. How would you handle such case?
+
+There is a more generic way of solving this problem.
+
+/*
+    Submission Date: 2016-12-06
+    Runtime: 95 ms
+    Difficulty: EASY
+*/
+
+using namespace std;
+#include <iostream>
+#include <cmath>
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x < 0){ return false; }
+        int i = floor(log10(x));
+        while(i > 0) {
+                return false;
+            }
+            x %= (int)pow(10, i);
+            x /= 10;
+            i -= 2;
+        }
+        return true;
+    }
+};
+
+int main() {
+    Solution s;
+    int input = -2147483648;
+    cout << s.isPalindrome(input) << endl;
+    return 0;
+}
--- test.cpp
+++ test.cpp
@@ -1,7 +1,29 @@
 using namespace std;
 #include <iostream>
+#include <cmath>
+
+class Solution {
+public:
+	bool isPalindrome(int x) {
+ 		if(x < 0){ return false; }
+    	int i = floor(log10(x));
+    	while(i > 0) {
+    		int msn = floor(x / pow(10, i));
+    		int lsn = x % 10;
+    		if(msn != lsn) {
+    			return false;
+    		}
+    		x %= (int)pow(10, i);
+    		x /= 10;
+    		i -= 2;
+    	}
+    	return true;
+    }
+};
 
 int main() {
-	cout << abs(-1) << endl;
+	Solution s;
+	int input = -2147483648;
+	cout << s.isPalindrome(input) << endl;
 	return 0;
 }

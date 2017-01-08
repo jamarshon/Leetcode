a725f225e33042db1011520926caebd2bd386de8
jamarshon <jasonl96@gmail.com>
2017-01-08 02:02:59 -0500

    LeetCode#25} Reverse Nodes in k-Group

--- 20-29/25ReverseNodesinkGroup.cpp
+++ 20-29/25ReverseNodesinkGroup.cpp
@@ -1,21 +1,34 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
+/*
+25. Reverse Nodes in k-Group
+Given a linked list, reverse the nodes of a linked list k at a time and return its modified list.
+
+k is a positive integer and is less than or equal to the length of the linked list. If the number of nodes is not a multiple of k then left-out nodes in the end should remain as it is.
+
+You may not alter the values in the nodes, only nodes itself may be changed.
+
+Only constant memory is allowed.
+
+For example,
+Given this linked list: 1->2->3->4->5
+
+For k = 2, you should return: 2->1->4->3->5
+
+For k = 3, you should return: 3->2->1->4->5
+
+/*
+    Submission Date: 2017-01-08
+    Runtime: 22 ms
+    Difficulty: HARD
+*/
+
 using namespace std;
 #include <iostream>
-#include <vector>
-#include <string>
 
 struct ListNode {
     int val;
-    ListNode* next;
+    ListNode *next;
     ListNode(int x) : val(x), next(NULL) {}
- };
+};
 
 class Solution {
 public:
@@ -74,23 +87,5 @@ public:
 };
 
 int main() {
-    Solution s;
-    ListNode* head = new ListNode(1);
-    ListNode* current = head;
-
-    vector<int> v{2,3,4, 5, 6};
-    for(int e : v) {
-        ListNode* newNode = new ListNode(e);
-        current -> next = newNode;
-        current = newNode;
-    }
-   
-    ListNode* b = s.reverseKGroup(head, 3);
-    ListNode* current2 = b;
-    while(current2 != NULL) {
-        cout << current2 -> val << "->";
-        current2 = current2 -> next;
-    }
-    cout << endl;
     return 0;
 }

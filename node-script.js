6d84f3b1705a981d51d46aad577ff9ed47febf96
jamarshon <jasonl96@gmail.com>
2017-01-20 13:17:48 -0500

    Creating a script to generate README.md

---
+++ node-script.js
@@ -0,0 +1,33 @@
+var fs = require('fs');
+var path = require('path');
+
+function getDirectories(srcpath) {
+  return fs.readdirSync(srcpath).filter(function(file) {
+    return fs.statSync(path.join(srcpath, file)).isDirectory();
+  });
+}
+
+var directories = getDirectories('.');
+var numericFolders = directories.reduce(function(memo, dir) {
+	if(dir.match(/[0-9]/g)) memo.push(dir);
+	return memo;
+}, []);
+
+var files = numericFolders.reduce(function(memo, folder){
+	var files = fs.readdirSync(folder);
+	files.forEach(function(file){
+		if(file.indexOf('.exe') !== -1) return;
+		var data = fs.readFileSync(folder + '/' + file, 'utf8')
+		memo.push(data.split('\r\n')[1]);
+	});
+	return memo;
+}, []);
+
+ var base = '# leetcode \r\n' +
+						'Ongoing solutions to leetcode in C++ \r\n \r\n' +
+						'Completed questions: \r\n';
+
+var message = base + files.join('\r\n');
+fs.writeFile('README.md', message, function(err){
+	if(err) throw err;
+})

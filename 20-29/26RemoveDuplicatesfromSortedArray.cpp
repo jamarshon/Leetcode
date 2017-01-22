4f68e08e98055979d1fc045b5845a80de913a631
jamarshon <jasonl96@gmail.com>
2017-01-22 14:06:25 -0500

    {LeetCode#26} Remove Duplicates from Sorted Array

---
+++ 20-29/26RemoveDuplicatesfromSortedArray.cpp
@@ -0,0 +1,39 @@
+/*
+26. Remove Duplicates from Sorted Array
+
+Given a sorted array, remove the duplicates in place such that each element appear only once and return the new length.
+
+Do not allocate extra space for another array, you must do this in place with constant memory.
+
+For example,
+Given input array nums = [1,1,2],
+
+Your function should return length = 2, with the first two elements of nums being 1 and 2 respectively. It doesn't matter what you leave beyond the new length.
+
+/*
+    Submission Date: 2017-01-22
+    Runtime: 48 ms
+    Difficulty: EASY
+*/
+
+using namespace std;
+#include <iostream>
+#include <vector>
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int i = 0, j = 1, len = nums.size();
+        if(len == 0) return 0;
+        
+        while(true) {
+            while(j < len && nums[i] == nums[j]) j++;
+            if(j == len) return i + 1;
+            swap(nums[++i], nums[j++]);
+        }
+    }
+};
+
+int main() {
+    return 0;
+}
--- test.cpp
+++ test.cpp
@@ -1,55 +1,25 @@
 using namespace std;
 #include <iostream>
-#include <unordered_map>
-#include <string>
+#include <vector>
 
 class Solution {
 public:
-    unordered_map<char, int> MAP = {
-       { '0', 0},
-        {'1', 1}
-    };
-    string addBinary(string a, string b) {
-        string result = "";
-        int i = a.size() - 1;
-        int j = b.size() - 1;
-        int carry = 0;
-        int aCurrent, bCurrent;
+    int removeDuplicates(vector<int>& nums) {
+        int i = 0, j = 1, len = nums.size();
+        if(len == 0) return 0;
         
-        while(i >= 0 && j >= 0) {
-            aCurrent = MAP[a[i]];
-            bCurrent = MAP[b[j]];
-            result = to_string(aCurrent ^ bCurrent ^ carry) + result;
-            carry = aCurrent & bCurrent | aCurrent & carry | bCurrent & carry;
-            i--;
-            j--;
+        while(true) {
+            while(j < len && nums[i] == nums[j]) j++;
+            if(j == len - 1) return i + 1;
+            swap(nums[++i], nums[j++]);
         }
-
-        while(i >= 0) {
-            aCurrent = MAP[a[i]];
-            result = to_string(aCurrent ^ carry) + result;
-            carry &= aCurrent;
-            i--;
-        }
-
-        while(j >= 0) {
-            bCurrent = MAP[b[j]];
-            result = to_string(bCurrent ^ carry) + result;
-            carry &= bCurrent;
-            j--;
-        }
-
-        if(carry > 0) result = "1" + result;
-        return result;
     }
 };
+
 int main() {
-    // Solution s;
-    string s = "hello";
-    string enclosed = "(" + s + ")";
-    switch(1) {
-    case 1 : cout << '1'; // prints "1",
-    case 2 : cout << '2'; // then prints "2"
-}
+   Solution s;
+   vector<int> v = {1,1,1,2,2,3};
+   s.removeDuplicates(v);
+   for(int i: v) cout << i << endl;
     return 0;
 }

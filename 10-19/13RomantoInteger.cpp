e184d7373c6a0c7609decb73a30544b9fa81f9dd
jamarshon <jasonl96@gmail.com>
2017-01-25 12:27:16 -0500

    {LeetCode#13} Roman to Integer

---
+++ 10-19/13RomantoInteger.cpp
@@ -0,0 +1,65 @@
+/*
+13. Roman to Integer
+
+Given a roman numeral, convert it to an integer.
+
+Input is guaranteed to be within the range from 1 to 3999.
+
+/*
+    Submission Date: 2017-01-25
+    Runtime: 172 ms
+    Difficulty: EASY
+*/
+
+using namespace std;
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+struct Denomination {
+    int value;
+    string symbol;
+    int numChar;
+};
+
+class Solution {
+public:
+    unordered_map<char, vector<Denomination>> CHAR_TO_DENOMINATIONS_MAP = {
+        {'M', {Denomination{1000, "M", 1}}},
+        {'D', {Denomination{500, "D", 1}}},
+        {'C', {Denomination{900, "CM", 2}, Denomination{400, "CD", 2}, Denomination{100, "C", 1}}},
+        {'L', {Denomination{50, "L", 1}}},
+        {'X', {Denomination{90, "XC", 2}, Denomination{40, "XL", 2}, Denomination{10, "X", 1}}},
+        {'V', {Denomination{5, "V", 1}}},
+        {'I', {Denomination{9, "IX", 2}, Denomination{4, "IV", 2}, Denomination{1, "I", 1}}}
+    };
+
+    int romanToInt(string s) {
+        int result = 0, 
+        		i = 0,
+        		len = s.size();
+
+        vector<Denomination> possibleDenominations;
+        Denomination denomination;
+
+        while(i < len) {
+	        	possibleDenominations = CHAR_TO_DENOMINATIONS_MAP[s[i]];
+	        	for(int j = 0, jLen = possibleDenominations.size(); j < jLen; j++) {
+		        		denomination = possibleDenominations[j];
+		        		if(s.substr(i, denomination.numChar) == denomination.symbol) {
+			        			result += denomination.value;
+			        			i += denomination.numChar;
+			        			break;
+		        		}
+	        	}
+        }
+
+        return result;
+    }
+};
+
+int main() {
+		Solution s;
+		cout << s.romanToInt("MCMXCVI") << endl;
+    return 0;
+}
--- test.cpp
+++ test.cpp
@@ -1,33 +1,17 @@
 using namespace std;
 #include <iostream>
-#include <vector>
+#include <map>
+#include <unordered_map>
 #include <algorithm>
 
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        int len = nums.size();
-        if(len <= 1) return;
         
-        vector<int>::reverse_iterator ceilIt, endIt;
-        for(int i = 0; i < len; i++) {
-            endIt = nums.rbegin() + i;
-            ceilIt = upper_bound(nums.rbegin(), endIt, nums[len - i - 1]);
-            if(ceilIt != endIt) {
-              swap(nums[len - i - 1], *ceilIt);
-              for(int t : nums) cout << t << endl;
-              sort(nums.rbegin(), endIt, greater<int>());
-              for(int t : nums) cout << t << endl;
-              return;
-            }
-        }
-        
-        reverse(nums.begin(), nums.end());
     }
 };
 int main() {
-    Solution s;
-    vector<int> v{1,3,2};
-    s.nextPermutation(v);
+    string t = "hello";
+    cout << t.substr(2,1) <<endl;
     return 0;
 }

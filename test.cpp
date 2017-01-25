6f25008bedf7cfe8290e7d6383b02ffde405a8b6
jamarshon <jasonl96@gmail.com>
2017-01-25 17:26:52 -0500

    {LeetCode#27} Remove Element

--- test.cpp
+++ test.cpp
@@ -6,12 +6,41 @@ using namespace std;
 
 class Solution {
 public:
-    void nextPermutation(vector<int>& nums) {
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        int len = nums.size();
+        if(len < 3) return {{}};
         
+        vector<vector<int>> result;
+        sort(nums.begin(), nums.end());
+        for(int b: nums) cout << b; cout << endl;
+        for(int i = 1; i < len; i++) {
+            int low = i + 1;
+            int high = len - 1;
+            int target = -1*nums[i];
+            while(low < high) {
+                if(nums[low] + nums[high] == target) {
+                    result.push_back({target, nums[low], nums[high]});
+                    low++;
+                    high--;
+                    
+                } else if(nums[low] + nums[high] > target) {
+                    high--;
+                } else {
+                    low++;
+                }
+            }
+            break;
+        }
+        
+        return result;
     }
 };
 int main() {
-    string t = "hello";
-    cout << t.substr(2,1) <<endl;
+    Solution s;
+    vector<int> v{-1,0,1,2,-1,-4};
+    vector<vector<int>> c = s.threeSum(v);
+    for(auto t: c){
+      for(int e: t) cout << e; cout << endl;
+    }
     return 0;
 }

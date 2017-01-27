dcf9f762ccf189ad92e17240b77ccee0d0d76fd8
jamarshon <jasonl96@gmail.com>
2017-01-27 13:37:34 -0500

    {LeetCode#98} Validate Binary Search Tree

---
+++ 90-99/98ValidateBinarySearchTree.cpp
@@ -0,0 +1,62 @@
+/*
+98. Validate Binary Search Tree
+Given a binary tree, determine if it is a valid binary search tree (BST).
+
+Assume a BST is defined as follows:
+
+The left subtree of a node contains only nodes with keys less than the node's key.
+The right subtree of a node contains only nodes with keys greater than the node's key.
+Both the left and right subtrees must also be binary search trees.
+
+    2
+   / \
+  1   3
+
+Binary tree [2,1,3], return true.
+
+    1
+   / \
+  2   3
+
+Binary tree [1,2,3], return false.
+
+/*
+    Submission Date: 2017-01-27
+    Runtime: 13 ms
+    Difficulty: MEDIUM
+*/
+
+using namespace std;
+#include <iostream>
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+ 
+class Solution {
+public:
+    bool isValidBSTHelper(TreeNode* root, int* min, int* max) {
+        if(root == NULL) return true;
+        
+        int rootVal = root -> val;
+        int* rootValPtr = &rootVal;
+        
+        if((min && rootVal <= *min) ||(max && rootVal >= *max)) return false;
+        
+        bool validLeftSubTree = isValidBSTHelper(root -> left, min, rootValPtr);
+        bool validRightSubTree = isValidBSTHelper(root -> right, rootValPtr, max);
+        
+        return validLeftSubTree && validRightSubTree;
+    }
+    
+    bool isValidBST(TreeNode* root) {
+        return isValidBSTHelper(root, NULL, NULL);
+    }
+};
+
+int main() {
+    return 0;
+}
--- test.cpp
+++ test.cpp
@@ -6,41 +6,46 @@ using namespace std;
 
 class Solution {
 public:
-    vector<vector<int>> threeSum(vector<int>& nums) {
-        int len = nums.size();
-        if(len < 3) return {{}};
+    // vector<vector<int>> threeSum(vector<int>& nums) {
+    //     int len = nums.size();
+    //     if(len < 3) return {{}};
         
-        vector<vector<int>> result;
-        sort(nums.begin(), nums.end());
-        for(int b: nums) cout << b; cout << endl;
-        for(int i = 1; i < len; i++) {
-            int low = i + 1;
-            int high = len - 1;
-            int target = -1*nums[i];
-            while(low < high) {
-                if(nums[low] + nums[high] == target) {
-                    result.push_back({target, nums[low], nums[high]});
-                    low++;
-                    high--;
+    //     vector<vector<int>> result;
+    //     sort(nums.begin(), nums.end());
+    //     for(int b: nums) cout << b; cout << endl;
+    //     for(int i = 1; i < len; i++) {
+    //         int low = i + 1;
+    //         int high = len - 1;
+    //         int target = -1*nums[i];
+    //         while(low < high) {
+    //             if(nums[low] + nums[high] == target) {
+    //                 result.push_back({target, nums[low], nums[high]});
+    //                 low++;
+    //                 high--;
                     
-                } else if(nums[low] + nums[high] > target) {
-                    high--;
-                } else {
-                    low++;
-                }
-            }
-            break;
-        }
+    //             } else if(nums[low] + nums[high] > target) {
+    //                 high--;
+    //             } else {
+    //                 low++;
+    //             }
+    //         }
+    //         break;
+    //     }
         
-        return result;
-    }
+    //     return result;
+    // }
 };
 int main() {
     Solution s;
-    vector<int> v{-1,0,1,2,-1,-4};
-    vector<vector<int>> c = s.threeSum(v);
-    for(auto t: c){
-      for(int e: t) cout << e; cout << endl;
-    }
+    // vector<int> v{-1,0,1,2,-1,-4};
+    // vector<vector<int>> c = s.threeSum(v);
+    // for(auto t: c){
+    //   for(int e: t) cout << e; cout << endl;
+    // }
+    cout << INT_MAX;
+    int *p = (int *)(1000);
+    int a = 1000;
+    int* c = new int(1000);
+    cout << *c;
     return 0;
 }

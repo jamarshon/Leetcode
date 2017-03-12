634467a1210d6563841f246b4582d5a8202b01ce
jamarshon <jasonl96@gmail.com>
2017-03-11 23:51:17 -0500

    {LeetCode#541} Reverse String II

---
+++ LeetCode Weekly Contest 23/541ReverseStringII.cpp
@@ -0,0 +1,44 @@
+/*
+541. Reverse String II
+Given a string and an integer k, you need to reverse the first k characters for every 2k characters counting from the start of the string. If there are less than k characters left, reverse all of them. If there are less than 2k but greater than or equal to k characters, then reverse the first k characters and left the other as original.
+
+Example:
+Input: s = "abcdefg", k = 2
+Output: "bacdfeg"
+
+Restrictions:
+The string consists of lower English letters only.
+Length of the given string and k will in the range [1, 10000]
+
+/*
+    Submission Date: 2017-03-11
+    Runtime: 26 ms
+    Difficulty: EASY
+*/
+
+using namespace std;
+#include <iostream>
+
+class Solution {
+public:
+    string reverseStr(string s, int k) {
+        string finalStr = "";
+        bool reverse = true;
+        int i = 0, len = s.size();
+        while(i < len) {
+            string currentStr = string(1, s[i++]);
+            while(i%k != 0 && i < len) {
+                currentStr = reverse ? s[i] + currentStr : currentStr + s[i];
+                i++;
+            }
+            finalStr += currentStr;
+            reverse ^= true;
+        }
+        
+        return finalStr;
+    }
+};
+
+int main() {
+    return 0;
+}
--- test.cpp
+++ test.cpp
@@ -7,16 +7,27 @@ using namespace std;
 
 class Solution {
 public:
-    unordered_map<char, int> m = {{'a',2},{'b',3},{'c',5},{'d',7},{'e',11},{'f',13},{'g',17},{'h',19},{'i',23},{'j',29},{'k',31},{'l',37},{'m',41},{'n',43},{'o',47},{'p',53},{'q',59},{'r',61},{'s',67},{'t',71},{'u',73},{'v',79},{'w',83},{'x',89},{'y',97},{'z',101}};
-    vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        return {};
+    string reverseStr(string s, int k) {
+        string finalStr = "";
+        bool reverse = true;
+        int i = 0, len = s.size();
+        while(i < len) {
+            string currentStr = string(1, s[i++]);
+            while(i%k != 0 && i < len) {
+                currentStr = reverse ? s[i] + currentStr : currentStr + s[i];
+                i++;
+            }
+            finalStr += currentStr;
+            reverse ^= reverse;
+        }
+        
+        return finalStr;
     }
 };
 
-
 int main() {
     Solution s;
-    // cout << s.findMaxLength({0,1,0});
+    cout << s.reverseStr("abcdefg", 2);
     
     return 0;
 }

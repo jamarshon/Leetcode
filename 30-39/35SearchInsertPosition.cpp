a2c7956277a078c69e6a649099ff731dec5b6b21
jamarshon <jasonl96@gmail.com>
2017-06-19 20:50:00 -0400

    35. Search Insert Position

---
+++ 30-39/35SearchInsertPosition.cpp
@@ -0,0 +1,47 @@
+/*
+35. Search Insert Position
+Given a sorted array and a target value, return the index if the target is found. 
+If not, return the index where it would be if it were inserted in order.
+
+You may assume no duplicates in the array.
+
+Here are few examples.
+[1,3,5,6], 5 → 2
+[1,3,5,6], 2 → 1
+[1,3,5,6], 7 → 4
+[1,3,5,6], 0 → 0
+
+/*
+    Submission Date: 2017-06-19
+    Runtime: 9 ms
+    Difficulty: EASY
+*/
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int len = nums.size();
+
+        int low = 0;
+        int high = len - 1;
+
+        while(low <= high) {
+            int mid = low + (high - low)/2;
+            if(nums[mid] == target) return mid;
+            else if(nums[mid] > target) high = mid - 1;
+            else low = mid + 1;
+        }
+
+        return low;
+    }
+};
+
+int main() {
+    Solution s;
+    return 0;
+}

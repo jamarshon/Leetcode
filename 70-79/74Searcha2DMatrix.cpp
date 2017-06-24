86ff57e2757dac487726ea077af450c787afd091
jamarshon <jasonl96@gmail.com>
2017-06-23 20:37:22 -0400

    74. Search a 2D Matrix

---
+++ 70-79/74Searcha2DMatrix.cpp
@@ -0,0 +1,56 @@
+/*
+74. Search a 2D Matrix
+Write an efficient algorithm that searches for a value in an m x n matrix. This matrix has 
+the following properties:
+
+Integers in each row are sorted from left to right.
+The first integer of each row is greater than the last integer of the previous row.
+For example,
+
+Consider the following matrix:
+
+[
+  [1,   3,  5,  7],
+  [10, 11, 16, 20],
+  [23, 30, 34, 50]
+]
+Given target = 3, return true.
+
+/*
+    Submission Date: 2017-06-23
+    Runtime: 9 ms
+    Difficulty: MEDIUM
+*/
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int M = matrix.size();
+        if(M == 0) return false;
+        int N = matrix[0].size();
+
+        int j = N - 1;
+        for(int i = 0; i < M; i++) {
+            for(; j >= 0; j--) {
+                if(matrix[i][j] == target) return true;
+                else if(matrix[i][j] < target) {
+                    // skip row
+                    break;
+                } else {
+                    continue;
+                }
+            }
+        }
+
+        return false;
+    }
+};
+
+int main() {
+    return 0;
+}

977f7ee77d69b66352204df05d71f9a98d66269e
jamarshon <jasonl96@gmail.com>
2017-07-18 21:38:12 -0400

    143. Reorder List

--- 100-109/108ConvertSortedArraytoBinarySearchTree.cpp
+++ 100-109/108ConvertSortedArraytoBinarySearchTree.cpp
@@ -25,12 +25,13 @@ public:
     TreeNode* sortedArrayToBST(vector<int>& nums, int start, int end) {
         if(start > end) return NULL;
         int mid = start + (end - start + 1)/2;
-        // cout << mid << ' ' << start << ' ' << end << endl;
+
         TreeNode* root = new TreeNode(nums[mid]);
         root -> left = sortedArrayToBST(nums, start, mid - 1);
         root -> right = sortedArrayToBST(nums, mid + 1, end);
         return root;
     }
+    
     TreeNode* sortedArrayToBST(vector<int>& nums) {
         return sortedArrayToBST(nums, 0, nums.size() - 1);
     }
---
+++ 140-149/143ReorderList.cpp
@@ -0,0 +1,78 @@
+/*
+143. Reorder List
+Given a singly linked list L: L0?L1?…?Ln-1?Ln,
+reorder it to: L0?Ln?L1?Ln-1?L2?Ln-2?…
+
+You must do this in-place without altering the nodes' values.
+
+For example,
+Given {1,2,3,4}, reorder it to {1,4,2,3}.
+
+/*
+    Submission Date: 2017-07-18
+    Runtime: 59 ms
+    Difficulty: MEDIUM
+*/
+
+#include <iostream>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    void reorderList(ListNode* head) {
+        // find midpoint
+        ListNode* slow = head;
+        ListNode* fast = head;
+        ListNode* last_slow = slow;
+        while(fast && fast -> next) {
+            last_slow = slow;
+            slow = slow -> next;
+            fast = fast -> next -> next;
+        }
+
+        if(slow == fast) return;
+
+        last_slow -> next = NULL;
+
+        // reverse the second half
+        ListNode* prev = NULL;
+        ListNode* curr = slow;
+        ListNode* new_curr = curr -> next;
+        while(curr) {
+            curr -> next = prev;
+            prev = curr;
+            curr = new_curr;
+            if(new_curr) new_curr = new_curr -> next;
+        }
+
+        // merge first half and second half
+        curr = prev;
+        ListNode* new_head = NULL;
+        new_curr = NULL;
+        while(head) {
+            new_head = head -> next;
+            new_curr = curr -> next;
+
+            head -> next = curr;
+            if(new_head) {
+                curr -> next = new_head;
+            } else if(new_curr) {
+                curr -> next = new_curr;
+            }
+
+            head = new_head;
+            curr = new_curr;
+        }
+    }
+};
+
+int main() {
+    return 0;
+}

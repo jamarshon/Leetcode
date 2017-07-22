bb10f5d239ced5185d7b030b42041a01ba1169f1
jamarshon <jasonl96@gmail.com>
2017-07-21 22:18:39 -0400

    141. Linked List Cycle

---
+++ 140-149/141LinkedListCycle.cpp
@@ -0,0 +1,41 @@
+/*
+141. Linked List Cycle
+Given a linked list, determine if it has a cycle in it.
+
+Follow up:
+Can you solve it without using extra space?
+
+/*
+    Submission Date: 2017-07-21
+    Runtime: 9 ms
+    Difficulty: EASY
+*/
+#include <iostream>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode* slow, *fast;
+        slow = fast = head;
+        while(fast && fast -> next) {
+            slow = slow -> next;
+            fast = fast -> next -> next;
+            if(slow == fast) return true;
+        }
+
+        return false;
+    }
+};
+
+int main() {
+    Solution s;
+    return 0;
+}

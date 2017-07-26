7cd888f2bf8c3b6e44bbba03c8ab0a7c1b5143b3
jamarshon <jasonl96@gmail.com>
2017-07-26 19:59:51 -0400

    160. Intersection of Two Linked Lists

---
+++ 160-169/160IntersectionofTwoLinkedLists.cpp
@@ -0,0 +1,76 @@
+/*
+160. Intersection of Two Linked Lists
+TWrite a program to find the node at which the intersection of two singly linked lists begins.
+
+
+For example, the following two linked lists:
+
+A:          a1 → a2
+                   ↘
+                     c1 → c2 → c3
+                   ↗            
+B:     b1 → b2 → b3
+begin to intersect at node c1.
+
+
+Notes:
+
+If the two linked lists have no intersection at all, return null.
+The linked lists must retain their original structure after the function returns.
+You may assume there are no cycles anywhere in the entire linked structure.
+Your code should preferably run in O(n) time and use only O(1) memory.
+
+/*
+    Submission Date: 2017-07-26
+    Runtime: 39 ms
+    Difficulty: EASY
+*/
+
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+#include <stack>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    int length(ListNode* head) {
+        int count = 0;
+        while(head) {
+            head = head -> next;
+            count++;
+        }
+
+        return count;
+    }
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        int length_A = length(headA);
+        int length_B = length(headB);
+
+        if(length_A > length_B) swap(headA, headB);
+
+        int diff = abs(length_B - length_A);
+        for(int i = 0; i < diff; i++) {
+            headB = headB -> next;
+        }
+
+        while(headA && headB) {
+            if(headA == headB) return headA;
+            headA = headA -> next;
+            headB = headB -> next;
+        }
+
+        return NULL;
+    }
+};
+
+int main() {
+    return 0;
+}

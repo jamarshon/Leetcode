56a612101be7588a1e997aad96f3f4e5912d7e0d
jamarshon <jasonl96@gmail.com>
2017-07-25 21:22:17 -0400

    201. Bitwise AND of Numbers Range

---
+++ 200-209/201BitwiseANDofNumbersRange.cpp
@@ -0,0 +1,43 @@
+/*
+201. Bitwise AND of Numbers Range
+Given a range [m, n] where 0 <= m <= n <= 2147483647, return the bitwise AND of all numbers in this range, 
+inclusive.
+
+For example, given the range [5, 7], you should return 4.
+
+/*
+    Submission Date: 2017-07-25
+    Runtime: 29 ms
+    Difficulty: MEDIUM
+*/
+
+#include <iostream>
+#include <cmath>
+#include <climits>
+
+using namespace std;
+
+class Solution {
+public:
+    int rangeBitwiseAnd(int m, int n) {
+        int res = 0;
+        for(int i = 30; i >= 0; i--) {
+            int shift = 1 << i;
+            if((shift & m) == (shift & n)) {
+                res |= shift & m;
+            } else {
+                break;
+            }
+        }
+        return res;
+    }
+};
+
+int main() {
+    Solution s;
+    cout << s.rangeBitwiseAnd(5, 7) << endl;
+    cout << s.rangeBitwiseAnd(5, 5) << endl;
+    cout << s.rangeBitwiseAnd(INT_MAX, INT_MAX-1) << endl;
+    cout << s.rangeBitwiseAnd(0, 0) << endl;
+    return 0;
+}

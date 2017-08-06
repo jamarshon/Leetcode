001fff8faab077f4c42c8d84b7304395a37763d7
jamarshon <jasonl96@gmail.com>
2017-08-06 15:26:01 -0400

    404. Sum of Left Leaves

---
+++ 400-409/404SumofLeftLeaves.cpp
@@ -0,0 +1,47 @@
+/*
+404. Sum of Left Leaves
+Find the sum of all left leaves in a given binary tree.
+
+Example:
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+
+There are two left leaves in the binary tree, with values 9 and 15 respectively. Return 24.
+
+/*
+    Submission Date: 2017-08-06
+    Runtime: 6 ms
+    Difficulty: EASY
+*/
+
+#include <iostream>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    int sumOfLeftLeaves(TreeNode* root) {
+        if(root == NULL) return 0;
+        int res = 0;
+        if(root -> left && root -> left -> left == NULL && root -> left -> right == NULL) {
+            res += root -> left -> val;
+        }
+        
+        return res + sumOfLeftLeaves(root -> left) + sumOfLeftLeaves(root -> right);
+    }
+};
+
+int main() {
+    return 0;
+}

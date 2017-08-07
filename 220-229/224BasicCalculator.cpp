d50a9e2e9b36877272f0bbee9f3d17ad7d9978ab
jamarshon <jasonl96@gmail.com>
2017-08-07 01:27:23 -0400

    224. Basic Calculator

---
+++ 220-229/224BasicCalculator.cpp
@@ -0,0 +1,110 @@
+/*
+224. Basic Calculator
+Implement a basic calculator to evaluate a simple expression string.
+
+The expression string may contain open ( and closing parentheses ), the plus + or minus sign -, 
+non-negative integers and empty spaces .
+
+You may assume that the given expression is always valid.
+
+Some examples:
+"1 + 1" = 2
+" 2-1 + 2 " = 3
+"(1+(4+5+2)-3)+(6+8)" = 23
+Note: Do not use the eval built-in library function.
+
+/*
+    Submission Date: 2017-08-07
+    Runtime: 26 ms
+    Difficulty: HARD
+*/
+
+#include <iostream>
+#include <algorithm>
+#include <cctype>
+#include <functional>
+#include <unordered_map>
+#include <stack>
+#include <cassert>
+
+using namespace std;
+
+class Solution {
+    unordered_map<char, function<int(int, int)>> ops_m{
+        {'+', [](const int a, const int b){ return a + b; }},
+        {'-', [](const int a, const int b){ return a - b; }},
+    };
+
+    unordered_map<char, char> toggle {{'+', '-'}, {'-', '+'}};
+public:
+    int calculate(string s) {
+        for(int i = 0, len = s.size(); i < len; i++) {
+            if(i > 0 && s[i-1] == '-' && s[i] == '(') {
+                // the idea is to keep track of "-(" and "("
+                // if we get to a ")" we pop. Maintain "-(" count in the stack
+                // if it is odd then toggle all '-' and '+' we come across
+                stack<char> stk;
+                int minus_open_count = 1;
+                stk.push('-');
+                while(!stk.empty() && ++i < len) {
+                    if(isspace(s[i]) || isdigit(s[i])) continue;
+                    if(s[i] == ')') {
+                        if(stk.top() == '-') minus_open_count--;
+                        stk.pop();
+                    } else if(s[i] == '(') {
+                        stk.push(')');
+                    } else if(s[i] == '+' || s[i] == '-') {
+                        bool should_push = s[i] == '-' && s[i+1] == '(';
+                        if(minus_open_count % 2 == 1) {
+                            s[i] = toggle[s[i]];
+                        }
+                        if(should_push) {
+                            stk.push('-');
+                            minus_open_count++;
+                            i++;
+                        }
+                    } else {
+                        assert(false);
+                    }
+                }
+            }
+        }
+        
+        s.erase(remove_if(s.begin(), s.end(), [](const char& c){
+            return isspace(c) || c == '(' || c == ')';
+        }), s.end());
+        
+        int N = s.size();
+        if(N == 0) return 0;
+
+        int res = 0;
+
+        function<int(int,int)> op = ops_m['+'];
+
+        for(int i = 0; i < N; i++) {
+            int start = i;
+            while(i < N && isdigit(s[i])) {
+                i++;
+            }
+
+            int val = stoi(s.substr(start, i - start));
+
+            res = op(res, val);
+            if(i < N) {
+                op = ops_m[s[i]];
+            }
+        }
+
+        return res;
+    }
+};
+
+int main() {
+    Solution s;
+    cout << s.calculate("1 + 1") << ' ' << 2 << endl;
+    cout << s.calculate(" 2-1 + 2 ") << ' ' << 3 << endl;
+    cout << s.calculate("(1+(4+5+2)-3)+(6+8)") << ' ' << 23 << endl;
+    cout << s.calculate("2-(5-6)") << ' ' << 3 << endl;
+    cout << s.calculate("(3-(2-(5-(9-(4)))))") << ' ' << 1 << endl;
+    return 0;
+}

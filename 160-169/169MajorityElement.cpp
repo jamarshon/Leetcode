bc404b375327555187e614a422b1142ec69b362a
jamarshon <jasonl96@gmail.com>
2017-08-14 23:22:48 -0400

    236. Lowest Common Ancestor of a Binary Tree

--- 160-169/169MajorityElement.cpp
+++ 160-169/169MajorityElement.cpp
@@ -12,10 +12,66 @@ You may assume that the array is non-empty and the majority element always exist
 #include <iostream>
 #include <vector>
 #include <unordered_map>
+#include <cassert>
 
 using namespace std;
 
 class Solution {
+public:
+/*
+Moore’s Voting Algorithm
+When applying the algorithm on an array, only one of below two cases might happen:
+
+A. the first candidate’s counter never drops to zero through out the array, or;
+B. the first candidate’s counter drops to zero at some point (reset point).
+
+If A, then apparently this candidate is the majority;
+
+If B, then (let’s say we have an array of n elements, and by the first time 
+counter drops to zero we have gone through x elements so far):
+
+    If the real majority element M never appeared in the subarray before the 
+    reset point, then it will still be the majority in the remaining subarray 
+    — do the algorithm again on remaining subarray;
+
+    If the majority element M has appeared in the subarray before reset 
+    point, then it must only have appeared up to x/2 times (because counter 
+    is now zero). Thus in remaining subarray we have (n-x) elements 
+    in total, of which at least (n/2 +1 – x/2) = (n-x)/2 +1 are M [to 
+    be more precise, it's at least (floor(n/2) +1 - x/2) = floor((n-x)/2)+1 ], 
+    making it still the majority in remaining subarray — do the 
+    algorithm again on remaining subarray;
+
+And there we have it like a recursive function. Note that this is when 
+there IS a majority (more than half) in the array. When there's no majority, 
+this process will give you a wrong candidate, that's why you always have to 
+do a second pass to check.
+    */
+    int majorityElement(vector<int>& nums) {
+        if(nums.empty()) return 0;
+
+        int res = nums.front();
+        int count = 1;
+        int N = nums.size();
+        
+        // find candidate
+        for(int i = 1; i < N; i++) {
+            count += nums[i] == res ? 1: -1;
+            if(count == 0) {
+                res = nums[i];
+                count = 1;
+            }
+        }
+
+        // confirm that this a majority
+        count = 0;
+        for(auto num: nums) count += num == res;
+        assert(count >= N/2);
+        return res;
+    }
+};
+
+class Solution2 {
 public:
     int majorityElement(vector<int>& nums) {
         unordered_map<int, int> value_to_freq;
---
+++ 230-239/236LowestCommonAncestorofaBinaryTree.cpp
@@ -0,0 +1,85 @@
+/*
+236. Lowest Common Ancestor of a Binary Tree
+Given a binary tree, find the lowest common ancestor (LCA) of two given nodes in 
+the tree.
+
+According to the definition of LCA on Wikipedia: “The lowest common ancestor is 
+defined between two nodes v and w as the lowest node in T that has both v and w 
+as descendants (where we allow a node to be a descendant of itself).”
+
+        _______3______
+       /              \
+    ___5__          ___1__
+   /      \        /      \
+   6      _2       0       8
+         /  \
+         7   4
+For example, the lowest common ancestor (LCA) of nodes 5 and 1 is 3. Another 
+example is LCA of nodes 5 and 4 is 5, since a node can be a descendant of 
+itself according to the LCA definition.
+
+/*
+    Submission Date: 2017-08-14
+    Runtime: 16 ms
+    Difficulty: MEDIUM
+*/
+#include <iostream>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    int lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q, TreeNode*& res) {
+        if(root == NULL) return 0;
+        
+        int count = 0;
+        if(root == p) count++;
+        if(root == q) count++;
+
+        if(count == 2) { // root == p == q so root is lca
+            res = root; 
+            return 2; 
+        }
+        
+        int left_count = lowestCommonAncestor(root -> left, p, q, res);
+        if(left_count == 2) return 2; // found p and q in root -> left
+        count += left_count;
+
+        // root is either p or q and the other is in the left tree so root is lca
+        if(count == 2) { 
+            res = root;
+            return 2;
+        }
+        
+        int right_count = lowestCommonAncestor(root -> right, p, q, res);
+        if(right_count == 2) return 2; // found p and q in root -> right
+        count += right_count;
+
+        // root and left_tree has either p or q and the other is in the right tree 
+        // so root is lca
+        if(count == 2) {
+            res = root;
+            return 2;
+        }
+        
+        return count; 
+    }
+    
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        TreeNode* res = NULL;
+        lowestCommonAncestor(root, p, q, res);
+        return res;
+    }
+};
+
+int main() {
+    Solution s;
+    return 0;
+}
